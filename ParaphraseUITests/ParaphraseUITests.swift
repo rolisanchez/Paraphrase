be5a25bba51affebac535c883ebed52fb73a866c
rolisanchez <roli.s91@gmail.com>
2020-06-20 23:54:11 +0800

    Add fastlane for screenshots

---
+++ ParaphraseUITests/ParaphraseUITests.swift
@@ -0,0 +1,41 @@
+//
+//  ParaphraseUITests.swift
+//  ParaphraseUITests
+//
+//  Created by Victor Rolando Sanchez Jara on 6/20/20.
+//  Copyright © 2020 Hacking with Swift. All rights reserved.
+//
+
+import XCTest
+
+class ParaphraseUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        snapshot("0Launch")
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
